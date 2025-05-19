8582e5e1b02d703e638417f991611787fba375e7
jedaqsaul <jedaqsaul@gmail.com>
2025-05-19 12:33:51 +0300

    Completes the python-p3-loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(i)
+        i-=1
+    print('Happy New Year!')
+        
+
+print(happy_new_year())
+    
+   
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    result=list()
+    for int in int_list:
+        square=int*int
+        result.append(square)
+        
+    return result
+
+print(square_integers([1,2,3, 4,5]))
+print('xxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range (1, 101):
+        if (i%3==0 and i%5==0):
+            print('FizzBuzz')
+        elif i%3==0:
+            print('Fizz')
+        elif i%5==0:
+            print('Buzz')
+        
+        else:
+            print(i)
+print(fizzbuzz())
